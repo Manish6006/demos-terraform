5ad2fd3cab4d133292734f596a8bd1067d392def
Manish6006 <manish.sharma591@gmail.com>
2024-06-23 14:00:21 +0530

    added new files

---
+++ base/variables.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+        region = "${var.region}"
+}
+[ec2-user@ip-172-31-41-4 base]$ cat variables.tf
+variable "prefix" {
+        type=string
+        default = "devops-tech"
+}
+
+variable "ami-id" {
+        type=string
+        description="this id of the ami to use to create ec2 instance"
+        default="ami-022ce6f32988af5fa"
+}
+
+variable "instance_type" {
+        type=string
+        description="this is define the instance type"
+        default="t2.micro"
+}
+
+variable "tags" {
+        type=string
+        description="this is define the tag to attached with ec2"
+        default="ec2FromTerraform"
+}
+
+variable "region" {
+        type=string
+        description="define the region to launch the instance"
+        default="ap-south-1"
+}
+
